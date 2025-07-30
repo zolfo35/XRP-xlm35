ea0bc69d11a5eef5cfbf9441ae69794302983106
zolfo35 <zolfosurfer@gmail.com>
2025-07-30 15:21:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,3 +14,18 @@ def echo_all(message):
 
 print("✅ Bot is running...")
 bot.polling()
+# --- Keep Replit alive ---
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is running"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+t = Thread(target=run)
+t.start()
